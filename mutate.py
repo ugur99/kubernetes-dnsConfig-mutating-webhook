9e8d1688873aab11de3d53bb3d81bfc503cc7c5d
Ugur Can Ozturk <57688057+ugur99@users.noreply.github.com>
2022-08-28 17:02:10 +0300

    fix

--- mutate.py
+++ mutate.py
@@ -2,73 +2,35 @@ from unittest.mock import patch
 from flask import Flask, request, jsonify
 from os import environ
 from logger import get_logger
-import incluster_config, base64, jsonpatch, copy, json
+import incluster_config, base64, jsonpatch, copy, json,random
 
 
 webhook = Flask(__name__)
 
-logger = get_logger()
-
+#logger = get_logger()
 
 @webhook.route('/mutate', methods=['POST'])
-def mutatating_webhook():
-  try:
-    request_json = json.loads(request.data.decode('utf-8'))
-  except Exception as e:
-    print(str(e))
+def mutate():
+    spec = request.json["request"]["object"]
+    modified_spec = copy.deepcopy(spec)
 
-  response = {
-    "apiVersion": "admission.k8s.io/v1",
-    "kind": "AdmissionReview",
-    "response": {
-      "uid": request_json['request']['uid'],
-      "allowed": True,
-    }
-  }
-
-  patch = """
-     [
-       {
-         "op": "add",
-         "path": "/spec/dnsConfig",
-         "value": {
-           "nameservers": [
-             "169.254.25.10"
-           ],
-           "options": [
-             {
-               "name": "timeout",
-               "value": "1"
-             },
-             {
-               "name": "ndots",
-               "value": "1"
-             },
-             {
-               "name": "attempts",
-               "value": "1"
-             }
-           ],
-           "searches": [
-             "svc.cluster.local"
-           ]
-         }
-       },
-       {
-         "op": "replace",
-         "path": "/spec/dnsPolicy",
-         "value": "None"
-       }
-     ]
-     """
-  patch_bytes = patch.encode('ascii')
-  patch_base64_bytes = base64.b64encode(patch_bytes)
-  patch_base64 = patch_base64_bytes.decode('ascii')
-
-  response['response']['patch'] = patch_base64
-  response['response']['patchType'] = "JSONPatch"
-
-  return json.dumps(response), 200, {'ContentType':'application/json-patch+json'} 
+    try:
+        modified_spec["metadata"]["labels"]["example.com/new-label"] = str(
+            random.randint(1, 1000)
+        )
+    except KeyError:
+        pass
+    patch = jsonpatch.JsonPatch.from_diff(spec, modified_spec)
+    return jsonify(
+        {
+            "response": {
+                "allowed": True,
+                "uid": request.json["request"]["uid"],
+                "patch": base64.b64encode(str(patch).encode()).decode(),
+                "patchtype": "JSONPatch",
+            }
+        }
+    )
 
 if __name__ == '__main__':
     webhook.run(host='0.0.0.0', port=5000)
