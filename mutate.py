200d9bd11adc1225c0271918f495878ee1451556
Ugur Can Ozturk <57688057+ugur99@users.noreply.github.com>
2022-08-28 19:32:18 +0300

    refactor

--- mutate.py
+++ mutate.py
@@ -12,66 +12,36 @@ logger = get_logger()
 
 @webhook.route('/mutate', methods=['POST'])
 def mutatating_webhook():
+
   try:
     request_json = json.loads(request.data.decode('utf-8'))
   except Exception as e:
     print(str(e))
 
-  response = {
-    "apiVersion": "admission.k8s.io/v1",
-    "kind": "AdmissionReview",
-    "response": {
-      "uid": request_json['request']['uid'],
-      "allowed": True,
-    }
-  }
+  uid = request_json['request']['uid']
 
   patch = "[{\"op\": \"add\", \"path\": \"/spec/dnsConfig\", \"value\": {\"nameservers\": [\"169.254.25.10\"], \"options\": [{\"name\": \"timeout\", \"value\": \"1\"}, {\"name\": \"ndots\", \"value\": \"1\"}, {\"name\": \"attempts\", \"value\": \"1\"}], \"searches\": [\"svc.cluster.local\"]}}, {\"op\": \"replace\", \"path\": \"/spec/dnsPolicy\", \"value\": \"None\"}]"
 
+  return mutatation_response(True, uid, patch)
 
-  #patch = """
-  #   [
-  #     {
-  #       "op": "add",
-  #       "path": "/spec/dnsConfig",
-  #       "value": {
-  #         "nameservers": [
-  #           "169.254.25.10"
-  #         ],
-  #         "options": [
-  #           {
-  #             "name": "timeout",
-  #             "value": "1"
-  #           },
-  #           {
-  #             "name": "ndots",
-  #             "value": "1"
-  #           },
-  #           {
-  #             "name": "attempts",
-  #             "value": "1"
-  #           }
-  #         ],
-  #         "searches": [
-  #           "svc.cluster.local"
-  #         ]
-  #       }
-  #     },
-  #     {
-  #       "op": "replace",
-  #       "path": "/spec/dnsPolicy",
-  #       "value": "None"
-  #     }
-  #   ]
-  #   """
-  patch_bytes = patch.encode('ascii')
-  patch_base64_bytes = base64.b64encode(patch_bytes)
-  patch_base64 = patch_base64_bytes.decode('ascii')
 
-  response['response']['patch'] = patch_base64
-  response['response']['patchType'] = "JSONPatch"
 
-  return json.dumps(response), 200, {'ContentType':'application/json-patch+json'} 
+def mutatation_response(allowed, uid, patch):
+   
+   response = {
+     "apiVersion": "admission.k8s.io/v1",
+     "kind": "AdmissionReview",
+     "response": {
+       "uid": uid,
+       "allowed": allowed,
+       "patch": base64.b64encode(patch.encode('ascii')).decode('ascii'),
+       "patchType": "JSONPatch"
+     }
+   }
+
+   return json.dumps(response), 200, {'ContentType':'application/json-patch+json'}
+
+
 
 if __name__ == '__main__':
     webhook.run(host='0.0.0.0', port=5000)
