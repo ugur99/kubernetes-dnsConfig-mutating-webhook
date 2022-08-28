874abf38c61366fd633aa273a42e2a6cedf7993b
Ugur Can Ozturk <57688057+ugur99@users.noreply.github.com>
2022-08-28 20:14:13 +0300

    fix

--- mutate.py
+++ mutate.py
@@ -9,21 +9,32 @@ webhook = Flask(__name__)
 
 logger = get_logger()
 
+properties = open("properties.yaml", "r")
+data = properties.read()
+list = data.split("\n")
+properties.close()
+
 
 @webhook.route('/mutate', methods=['POST'])
 def mutatating_webhook():
 
   try:
-    #request_json = json.loads(request.data.decode('utf-8'))
     request_json = request.get_json()
   except Exception as e:
     print(str(e))
 
   uid = request_json['request']['uid']
+  namespace = request_json["request"]["namespace"]
 
-  patch = "[{\"op\": \"add\", \"path\": \"/spec/dnsConfig\", \"value\": {\"nameservers\": [\"169.254.25.10\"], \"options\": [{\"name\": \"timeout\", \"value\": \"1\"}, {\"name\": \"ndots\", \"value\": \"2\"}, {\"name\": \"attempts\", \"value\": \"1\"}], \"searches\": [\"svc.cluster.local\",\"ns.svc.cluster.local\"]}}, {\"op\": \"replace\", \"path\": \"/spec/dnsPolicy\", \"value\": \"None\"}]"
-
-  return mutatation_response(True, uid, patch)
+  if namespace in list:
+    logger.info("Namespace " + namespace + "is not retricted.")
+    logger.debug("Request UID is: " + uid)
+    return default_response(uid)
+  else:
+    logger.info("DNS Mutation Webhhok is applying ...")
+    logger.debug("Request UID is: " + uid)
+    patch = "[{\"op\": \"add\", \"path\": \"/spec/dnsConfig\", \"value\": {\"nameservers\": [\"169.254.25.10\"], \"options\": [{\"name\": \"timeout\", \"value\": \"1\"}, {\"name\": \"ndots\", \"value\": \"2\"}, {\"name\": \"attempts\", \"value\": \"1\"}], \"searches\": [\"svc.cluster.local\",\"ns.svc.cluster.local\"]}}, {\"op\": \"replace\", \"path\": \"/spec/dnsPolicy\", \"value\": \"None\"}]"
+    return mutatation_response(True, uid, patch)
 
 
 
@@ -42,6 +53,19 @@ def mutatation_response(allowed, uid, patch):
 
    return json.dumps(response), 200, {'ContentType':'application/json-patch+json'}
 
+
+def default_response(uid):
+   
+   response = {
+     "apiVersion": "admission.k8s.io/v1",
+     "kind": "AdmissionReview",
+     "response": {
+       "uid": uid,
+       "allowed": True
+     }
+   }
+
+   return json.dumps(response), 200, {'ContentType':'application/json-patch+json'}
 
 
 if __name__ == '__main__':
